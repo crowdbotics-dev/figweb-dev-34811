7b0d9e5685a9c53c7f174a97d60c3170467a0b1b
Crowdbotics <engineering@crowdbotics.com>
2022-09-22 08:23:50 +0000

    Initial commit.

---
+++ figweb_dev_34811/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for figweb_dev_34811 project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'figweb_dev_34811.settings')
+
+application = get_wsgi_application()
